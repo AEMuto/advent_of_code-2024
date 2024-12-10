71ebc7e108dbcb698d8e482d6ccfb88ce2981bad
AEMuto <antoine.marseaud@gmail.com>
2024-12-10 11:03:19 +0100

    Clean up & explanations part1

--- src/day_09/main.ts
+++ src/day_09/main.ts
@@ -11,26 +11,20 @@ const input = file.split("").map((x) => parseInt(x, 10));
 // 00...111...2...333.44.5555.6666.777.888899
 // const input = "2333133121414131402".split("").map((x) => parseInt(x, 10));
 
-const to_blocks = (diskmap: number[]) => {
+const to_blocks = async (diskmap: number[]) => {
   const res = [];
   for (let i = 0; i < diskmap.length; i++) {
     // console.log(`\ni: ${i} | diskmap[i]: ${diskmap[i]}`);
-
     if (i % 2 === 0) {
       res.push(Array(diskmap[i]).fill(i / 2))
-      // console.log(`value: ${value} | diskmap[i]: ${diskmap[i]}`);
-      
     } else if (diskmap[i] !== 0) {
-      res.push(Array(diskmap[i]).fill("."))
-      // console.log(`value: ${value} | diskmap[i]: ${diskmap[i]}`);
-
+      res.push(Array(diskmap[i]).fill(".").join(""));
     }
   }
-  // console.log(res);
-  return res.flat();
+  // console.log(res); // look like [[51, 51],'.....']
+  // await writeFile("src/day_09/blockfiles.txt", block_files.join(""));
+  return res.flat(); // [51, 51, '.....']
 };
-
-// console.log(to_blocks(input));
 
 const calc_checksum = (input: Array<number>) => {
   let res = 0;
@@ -46,12 +40,10 @@ const findLastNumIndex = (block_files: (string | number)[]) => {
   }
 }
 
-
 const result_part1 = async (input: number[]) => {
-  const block_files = to_blocks(input)
-
-  await writeFile("src/day_09/blockfiles.txt", block_files.join(""));
-  console.log(`block_files length (original): ${block_files.length}`);
+  const blocks = await to_blocks(input);
+  const block_files = blocks.map((block) => typeof block === "string" ? block.split("") : block).flat();
+  // blocks: [51, 51, '.....'] -> block_files: [51, 51, '.', '.', '.', '.']
   const length = block_files.length;
   for (let i = 0; i < length; i++) {
     if (block_files[i] === ".") {
@@ -64,8 +56,8 @@ const result_part1 = async (input: number[]) => {
       }
     }
   }
-  console.log(`block_files length (sorted): ${block_files.length}`);
-  await writeFile("src/day_09/blockfiles_sorted.txt", block_files.join(""));
+  // console.log(`block_files length (sorted): ${block_files.length}`);
+  // await writeFile("src/day_09/blockfiles_sorted.txt", block_files.join(""));
   return calc_checksum(block_files.filter(x=>typeof x === "number"));
 };
 
@@ -82,61 +74,61 @@ console.timeEnd("part1");
 //   ),
 // );
 
-const result_part2 = (input: number[]) => {
-  const block_files = to_blocks(input);
-  const nums = block_files.filter((str) => !str.includes(".")).reverse(); // Help array
-  //console.log(block_files);
-  for (let i = 0; i < block_files.length; i++) {
-    // console.log(`### ROUND N°${i} ###`)
-    let current = block_files[i];
-    // console.log("current: ",current)
-    if (current.includes(".")) {
-      for (let j = 0; j < nums.length; j++) {
-        // Go through all nums, eg : 99,8888,777,6666,...
-        const last_num = nums[j];
-        // console.log("last_num: ",last_num)
-        if (current.length < last_num.length) {
-          // console.log("current < last_num", current, last_num);
-          continue;
-        } else {
-          // console.log("current > last_num", current, last_num);
-          nums.splice(j, 1); // Delete num from help array since we can insert
-          const last_num_index = block_files.lastIndexOf(last_num);
-          // numbers should be only moved from left to right so if the current index is greater than the last_num_index
-          // then we exit the loop
-          if (i > last_num_index) {
-            // console.log("i > last_num_index");
-            break;
-          }
-          const remove = block_files
-            .splice(
-              last_num_index, // Start index
-              1, // Replace
-              Array(last_num.length).fill(".").join(""), // With dots (free space)
-            )
-            .join("");
+// const result_part2 = (input: number[]) => {
+//   const block_files = to_blocks(input);
+//   const nums = block_files.filter((str) => !str.includes(".")).reverse(); // Help array
+//   //console.log(block_files);
+//   for (let i = 0; i < block_files.length; i++) {
+//     // console.log(`### ROUND N°${i} ###`)
+//     let current = block_files[i];
+//     // console.log("current: ",current)
+//     if (current.includes(".")) {
+//       for (let j = 0; j < nums.length; j++) {
+//         // Go through all nums, eg : 99,8888,777,6666,...
+//         const last_num = nums[j];
+//         // console.log("last_num: ",last_num)
+//         if (current.length < last_num.length) {
+//           // console.log("current < last_num", current, last_num);
+//           continue;
+//         } else {
+//           // console.log("current > last_num", current, last_num);
+//           nums.splice(j, 1); // Delete num from help array since we can insert
+//           const last_num_index = block_files.lastIndexOf(last_num);
+//           // numbers should be only moved from left to right so if the current index is greater than the last_num_index
+//           // then we exit the loop
+//           if (i > last_num_index) {
+//             // console.log("i > last_num_index");
+//             break;
+//           }
+//           const remove = block_files
+//             .splice(
+//               last_num_index, // Start index
+//               1, // Replace
+//               Array(last_num.length).fill(".").join(""), // With dots (free space)
+//             )
+//             .join("");
 
-          const diff = current.length - last_num.length;
-          const insert = `${diff > 0 ? Array(diff).fill(".").join("") : ""}`;
-          // console.log("insert: ", insert);
-          // console.log("before:", block_files)
-          insert.length > 0
-            ? block_files.splice(i, 1, remove, insert)
-            : block_files.splice(i, 1, remove);
-          // console.log("after:", block_files)
-          break;
-        }
-      }
-    }
-  }
-  // console.log(block_files);
-  const res = block_files
-    .join("")
-    .split("")
-    .map((x) => (x !== "." ? +x : x));
-  // console.log(res);
-  //return calc_checksum(res);
-};
+//           const diff = current.length - last_num.length;
+//           const insert = `${diff > 0 ? Array(diff).fill(".").join("") : ""}`;
+//           // console.log("insert: ", insert);
+//           // console.log("before:", block_files)
+//           insert.length > 0
+//             ? block_files.splice(i, 1, remove, insert)
+//             : block_files.splice(i, 1, remove);
+//           // console.log("after:", block_files)
+//           break;
+//         }
+//       }
+//     }
+//   }
+//   // console.log(block_files);
+//   const res = block_files
+//     .join("")
+//     .split("")
+//     .map((x) => (x !== "." ? +x : x));
+//   // console.log(res);
+//   //return calc_checksum(res);
+// };
 
 // console.time("part2");
 // console.log(result_part2(input_2));
