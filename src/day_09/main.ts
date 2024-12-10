b1375d4d250a1b57f7e64e85b7b253b3699af5fb
AEMuto <antoine.marseaud@gmail.com>
2024-12-10 15:43:19 +0100

    Finished day_09 part2

--- src/day_09/main.ts
+++ src/day_09/main.ts
@@ -16,21 +16,24 @@ const to_blocks = async (diskmap: number[]) => {
   for (let i = 0; i < diskmap.length; i++) {
     // console.log(`\ni: ${i} | diskmap[i]: ${diskmap[i]}`);
     if (i % 2 === 0) {
-      res.push(Array(diskmap[i]).fill(i / 2))
+      res.push(Array(diskmap[i]).fill(i / 2));
     } else if (diskmap[i] !== 0) {
       res.push(Array(diskmap[i]).fill(".").join(""));
     }
   }
   // console.log(res); // look like [[51, 51],'.....']
   // await writeFile("src/day_09/blockfiles.txt", block_files.join(""));
-  return res.flat(); // [51, 51, '.....']
+  return res;
 };
 
-const calc_checksum = (input: Array<number>) => {
+const calc_checksum = (input: Array<number | string>) => {
   let res = 0;
   for (let i = 0; i < input.length; i++) {
-    res += input[i] * i;
+    if (typeof input[i] === "number") {
+      res += (input[i] as number) * i;
+    }
   }
+
   return res;
 };
 
@@ -38,11 +41,14 @@ const findLastNumIndex = (block_files: (string | number)[]) => {
   for (let i = block_files.length - 1; i >= 0; i--) {
     if (typeof block_files[i] === "number") return i;
   }
-}
+};
 
 const result_part1 = async (input: number[]) => {
-  const blocks = await to_blocks(input);
-  const block_files = blocks.map((block) => typeof block === "string" ? block.split("") : block).flat();
+  const blocks = (await to_blocks(input)).flat(); // blocks: [[51, 51],'.....'] -> [51, 51, '.....']
+  const block_files = blocks
+    .map((block) => (typeof block === "string" ? block.split("") : block))
+    .flat();
+  await writeFile("src/day_09/blockfiles.txt", JSON.stringify(block_files));
   // blocks: [51, 51, '.....'] -> block_files: [51, 51, '.', '.', '.', '.']
   const length = block_files.length;
   for (let i = 0; i < length; i++) {
@@ -57,15 +63,13 @@ const result_part1 = async (input: number[]) => {
     }
   }
   // console.log(`block_files length (sorted): ${block_files.length}`);
-  // await writeFile("src/day_09/blockfiles_sorted.txt", block_files.join(""));
-  return calc_checksum(block_files.filter(x=>typeof x === "number"));
+  await writeFile("src/day_09/blockfiles_sorted.txt", JSON.stringify(block_files));
+  //const res = block_files.filter((x) => typeof x === "number");
+  return calc_checksum(block_files.filter((x) => typeof x === "number"));
 };
 
-const input_1 = structuredClone(input);
-const input_2 = structuredClone(input);
-
 console.time("part1");
-console.log(await result_part1(input_1));
+console.log(await result_part1(input));
 console.timeEnd("part1");
 
 // console.log(
@@ -74,62 +78,59 @@ console.timeEnd("part1");
 //   ),
 // );
 
-// const result_part2 = (input: number[]) => {
-//   const block_files = to_blocks(input);
-//   const nums = block_files.filter((str) => !str.includes(".")).reverse(); // Help array
-//   //console.log(block_files);
-//   for (let i = 0; i < block_files.length; i++) {
-//     // console.log(`### ROUND N°${i} ###`)
-//     let current = block_files[i];
-//     // console.log("current: ",current)
-//     if (current.includes(".")) {
-//       for (let j = 0; j < nums.length; j++) {
-//         // Go through all nums, eg : 99,8888,777,6666,...
-//         const last_num = nums[j];
-//         // console.log("last_num: ",last_num)
-//         if (current.length < last_num.length) {
-//           // console.log("current < last_num", current, last_num);
-//           continue;
-//         } else {
-//           // console.log("current > last_num", current, last_num);
-//           nums.splice(j, 1); // Delete num from help array since we can insert
-//           const last_num_index = block_files.lastIndexOf(last_num);
-//           // numbers should be only moved from left to right so if the current index is greater than the last_num_index
-//           // then we exit the loop
-//           if (i > last_num_index) {
-//             // console.log("i > last_num_index");
-//             break;
-//           }
-//           const remove = block_files
-//             .splice(
-//               last_num_index, // Start index
-//               1, // Replace
-//               Array(last_num.length).fill(".").join(""), // With dots (free space)
-//             )
-//             .join("");
+const result_part2 = async (input: number[]) => {
+  // block_files: [[51, 51],'.....']
+  const block_files = await to_blocks(input);
+  await writeFile("src/day_09/blockfiles_part2.txt", JSON.stringify(block_files));
+  // console.log("block_files (og):", block_files);
+  // console.log(`block_files (og) length: ${block_files.length}`);
+  const length = block_files.length;
 
-//           const diff = current.length - last_num.length;
-//           const insert = `${diff > 0 ? Array(diff).fill(".").join("") : ""}`;
-//           // console.log("insert: ", insert);
-//           // console.log("before:", block_files)
-//           insert.length > 0
-//             ? block_files.splice(i, 1, remove, insert)
-//             : block_files.splice(i, 1, remove);
-//           // console.log("after:", block_files)
-//           break;
-//         }
-//       }
-//     }
-//   }
-//   // console.log(block_files);
-//   const res = block_files
-//     .join("")
-//     .split("")
-//     .map((x) => (x !== "." ? +x : x));
-//   // console.log(res);
-//   //return calc_checksum(res);
-// };
+  /**
+   * Reverse loop: we take the last number and try to fit it into the first free space
+   */
+  let round = 0;
+  for (let right = length - 1; right >= 0; right--) {
+    // console.log(`\n### Round ${round} ###`);
+    // console.log(`Current right: ${right}`);
+    // console.log(`Current block_files: "${block_files}"`);
+    // console.log(`Current block_file: "${block_files[right]}"`);
+    if (!block_files[right].includes(".")) {
+      const num = block_files[right];
+      for (let left = 0; left < length && left < right; left++) {
+        // ! left < right
 
-// console.time("part2");
-// console.log(result_part2(input_2));
-// console.timeEnd("part2");
+        if (block_files[left].includes(".")) {
+          const free_space = block_files[left];
+          if (num.length <= free_space.length) {
+            const diff = free_space.length - num.length;
+            if (diff > 0) {
+              block_files[left] = num;
+              block_files[right] = Array(num.length).fill(".").join("");
+              block_files.splice(left + 1, 0, Array(diff).fill(".").join(""));
+              right++;
+            } else {
+              block_files[left] = num;
+              block_files[right] = Array(num.length).fill(".").join("");
+            }
+            break;
+          }
+        }
+      }
+    }
+    round++;
+  }
+
+  // console.log("block_files (sorted):", block_files);
+  // console.log(`block_files length (sorted): ${block_files.length}`);
+  // console.log(`First number: ${res[0]}, is it a number? ${typeof res[0] === "number"}`);
+  const res = block_files.flat().map((x) => typeof x === "string" && x.length > 1 ? x.split("") : x).flat();
+  await writeFile("src/day_09/blockfiles_sorted_part2.txt", JSON.stringify(res));
+  return calc_checksum(res);
+};
+
+console.time("part2");
+console.log(await result_part2(input));
+console.timeEnd("part2");
+
+// 6352041120068 too low
